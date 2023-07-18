ae6277f445c63ad89b9f73a73f2ff685f2b71601
Siva <badasiva9912@gmail.com>
2023-07-18 17:11:17 +0530

    New code

---
+++ react-application/src/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Search.css';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <input type="search" placeholder="Search..." className="Search" />
+
+        </div>
+    )
+}
+export default App;
